206616a462c87a77d7420b602425ad71a53d97a2
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:28:56 +0000

    Feat: Implemented controller endpoints for authentication

---
+++ HubWally.Api/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using HubWally.Application.Commands.Requests.Accounts;
+using HubWally.Application.DTOs.Accounts;
+using MediatR;  
+using Microsoft.AspNetCore.Mvc; 
+
+namespace HubWally.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;       
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (ModelState.IsValid)
+            { 
+                var result = await _mediator.Send(new RegisterCommand { registerDto = registerDto});
+
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest(ModelState);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _mediator.Send(new LoginCommand { loginDto = loginDto });
+
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest(ModelState);
+        } 
+    }
+
+}
