974ec451c85c1d9b8372cac9c1256d9357969ca3
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:18:07 +0000

    Feat: Added wallet model

---
+++ HubWally.Domain/Models/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using HubWally.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Domain.Models
+{
+    public class Wallet : BaseModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string? AccountNumber { get; set; }
+        public string AccountScheme { get; set; } 
+        public string Owner { get; set; }
+    }
+}
