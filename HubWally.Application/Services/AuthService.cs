9050bdab14dfb7069a153226eea4eaaf1a64cf63
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:23:51 +0000

    Feat: Implemented services for authentication

---
+++ HubWally.Application/Services/AuthService.cs
@@ -0,0 +1,70 @@
+﻿using HubWally.Application.DTOs.Accounts; 
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;  
+using System.Text;
+using HubWally.Application.Services.IServices;
+using Microsoft.AspNetCore.Identity; 
+using Microsoft.EntityFrameworkCore;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames; 
+
+namespace HubWally.Application.Services
+{
+    public class AuthService(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) : IAuthService
+    {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+
+        public async Task<IdentityResult> Register(RegisterDto model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.PhoneNumber,
+                PhoneNumber = model.PhoneNumber
+            };
+
+            return await _userManager.CreateAsync(user, model.Password);
+        }
+        public async Task<string?> Login(LoginDto model)
+        {
+            // Find the user by phone number
+            var user = await _userManager.Users.SingleOrDefaultAsync(u => u.PhoneNumber == model.PhoneNumber);
+
+            if (user != null)
+            {
+                // Attempt to sign in the user
+                var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+                if (result.Succeeded)
+                {
+                    return GenerateJwtToken(user);
+                }
+            }
+            return null;
+        }
+
+        private static string GenerateJwtToken(IdentityUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kd5 4kejqwlkejalkejaj439oejf,xcm")); //TODO: move secret key to appsettings.json
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "hubwally.com",
+                audience: "hubwally.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
---
+++ HubWally.Application/Services/IServices/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using HubWally.Application.DTOs.Accounts;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Application.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<IdentityResult> Register(RegisterDto model);
+        Task<string?> Login(LoginDto model);
+    }
+}
