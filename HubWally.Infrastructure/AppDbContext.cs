9c5f3b69f6beb839dd707a724f5f4bf1adf39812
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:21:43 +0000

    Feat: Added db context

---
+++ HubWally.Infrastructure/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using HubWally.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Infrastructure
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> opt) : IdentityDbContext(opt)
+    {
+        public DbSet<Wallet> Wallets { get; set; }
+    }
+}
