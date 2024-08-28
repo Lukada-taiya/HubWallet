8753f7fd53e9d00e5fc1010f46d26360d653f822
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:25:54 +0000

    Feat: Implemented services for wallet

---
+++ HubWally.Application/Services/IServices/IWalletService.cs
@@ -0,0 +1,19 @@
+﻿using HubWally.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Application.Services.IServices
+{
+    public interface IWalletService
+    {
+        Task<int> AddWallet(Wallet entity);
+        Task<IEnumerable<Wallet>> GetAllWallets();
+        Task<Wallet> GetWallet(int id);
+        Task<Wallet> GetWalletsByOwner(string phoneNumber);
+        Task<int> UpdateWallet(Wallet entity);
+        Task<int> DeleteWallet(int id);
+    }
+}
---
+++ HubWally.Application/Services/WalletService.cs
@@ -0,0 +1,40 @@
+﻿using HubWally.Application.Services.IServices;
+using HubWally.Domain.Models;
+using HubWally.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Application.Services
+{
+    public class WalletService(IBaseRepository<Wallet> repository) : IWalletService
+    {
+        private readonly IBaseRepository<Wallet> _repository = repository;
+        public Task<int> AddWallet(Wallet entity)
+        {
+            return _repository.Add(entity);
+        }
+
+        public Task<int> DeleteWallet(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        public Task<IEnumerable<Wallet>> GetAllWallets()
+        {
+            return _repository.GetAllAsync();
+        }
+
+        public Task<Wallet> GetWallet(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        public Task<int> UpdateWallet(Wallet entity)
+        {
+            return _repository.Update(entity);
+        }
+    }
+}
