ddb52f110d8bd9d1e09b2aeacd5120e3fba942b9
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-28 02:22:28 +0000

    Feat: Added generic repository

---
+++ HubWally.Infrastructure/Persistence/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Infrastructure.Persistence
+{ 
+    public interface IBaseRepository<TEntity>
+    {
+        Task<int> Add(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> Get(int id, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> Get(string columnName, object columnValue, params Expression<Func<TEntity, object>>[] includes);
+        Task<int> Update(TEntity entity);
+        Task<int> Delete(int id);
+    }    
+}
