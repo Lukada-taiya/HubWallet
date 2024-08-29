503378906df21312885001dd3e9e0d040df7b169
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-29 10:35:27 +0000

    Feat: Added dtos for authentication

---
+++ HubWally.Application/DTOs/Accounts/LoginDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubWally.Application.DTOs.Accounts
+{
+    public class LoginDto
+    {
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
