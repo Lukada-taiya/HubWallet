55984b9157e0e14b06a82ae2ac542c2004600e0e
Lukada-taiya <lukmanadam43@gmail.com>
2024-08-29 10:33:02 +0000

    Feat: Added automapper maps

---
+++ HubWally.Api/Configurations/Mappers.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HubWally.Application.DTOs.Wallets;
+using HubWally.Domain.Models;
+
+namespace HubWally.Api.Configurations
+{
+    public class Mappers : Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Wallet, WalletDto>().ReverseMap();
+            CreateMap<Wallet, GetWalletDto>().ReverseMap();
+        }
+    }
+}
